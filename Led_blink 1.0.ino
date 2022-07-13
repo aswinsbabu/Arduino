575012d043623804ebed138767d2aa3746588ce5
aswinsbabu <aswinsbabu007@gmail.com>
2022-07-13 09:49:30 +0100

    Add files via upload

---
+++ Led_blink 1.0.ino
@@ -0,0 +1,14 @@
+void setup() {
+
+ pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+/*  Rearrahne the following */
+digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+delay(1000); 
+
+
+}
