616f8e6accc48aca3c8e14a90b2e429cb02136c3
aswinsbabu <aswinsbabu007@gmail.com>
2023-10-06 14:53:46 +0530

    Update 3.11 Floor Cleaner RobotMotion.cpp

--- Qlearn/Projects/3.1 Floor Cleaner RobotMotion.cpp
+++ Qlearn/Projects/3.1 Floor Cleaner RobotMotion.cpp
@@ -4,12 +4,21 @@ Displayed all ultrasonic sensor values
 
 */
 //#include <NewPing.h> //for ultrasonic sensor
+//Ultrasonic Sensor Pins
 #define R_TRIG_PIN   2 // Trig pin of Right sensor 
 #define R_ECHO_PIN   3    // Echo pin of Right sensor 
 #define F_TRIG_PIN   4 // Trig pin of Front sensor 
 #define F_ECHO_PIN   5    // Echo pin of Front sensor 
 #define L_TRIG_PIN   6 // Trig pin of left sensor 
 #define L_ECHO_PIN   7    // Echo pin of left sensor 
+
+//Robot wheels
+#define LeftMtrInput1
+#define LeftMtrInput2
+#define RightMtrInput1
+#define RightMtrInput2
+#define LeftMtrEnable
+#define RightMtrEnable
 
 #define MAX_DISTANCE 200 // Maximum distance to consider (in cm)
 #define OBSTACLE_DISTANCE 20 // Distance to consider an obstacle (in cm)
@@ -22,8 +31,18 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
+pinMode(LeftMtrInput1, Output);
+pinMode(LeftMtrInput2 , Output);
 
-}
+pinMode(RightMtrInput1, Output);
+pinMode(RightMtrInput2, Output);
+
+
+pinMode(LeftMtrEnable, Output);
+pinMode(RightMtrEnable, Output);
+
+
+digitaWrite(LeftMtrInput1, High);}
 
 /////////////////////////////     MAIN    /////////////////////////////////////
 void loop() {
@@ -80,11 +99,18 @@ void display_ObstaceleDistance(){ // Displays all ultrasonic sensor values
 }
 ////////////// Functions For Robot motion
 void move_robotFrwd(){
-  
+digitaWrite(LeftMtrInput1, High);
+digitaWrite(LeftMtrInput2, Low);
+
+digitaWrite(RightMtrInput1, High);
+digitaWrite(RightMtrInput2, Low);
+analogWrite(
 }
 
 void move_robotRight(){
-  
+
+digitaWrite(LeftMtrInput1, High);
+digitaWrite(LeftMtrInput2, Low);  
 }
 void move_robotLeft(){
   
